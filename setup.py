e785b43408e1e6196343e0b0daecf26ff8b9dc2b
jeroyang <jeroyang@gmail.com>
2015-02-27 10:21:55 +0800

    make package

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+from setuptools import setup, find_packages
+
+setup(name='nlptoolkit',
+      version='0.1',
+      description='My minimal NLP tools for Biocreative IV contest',
+      author='Chia-Jung, Yang',
+      author_email='jeroyang@gmail.com',
+      packages=find_packages()
+)
+
